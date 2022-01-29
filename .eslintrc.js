bebba920a2051bb182fccfa92f08cae935ede257
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2022-01-29 22:53:00 +0900

    aa

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,32 +6,32 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
-    '@typescript-eslint/ban-types': [
-      'error',
+    "@typescript-eslint/ban-types": [
+      "error",
       {
         extendDefaults: true,
         types: {
-          '{}': false,
+          "{}": false,
         },
       },
     ],
